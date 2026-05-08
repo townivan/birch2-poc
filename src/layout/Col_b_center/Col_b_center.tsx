fdf1f4ffd11923fbf7f69a02dcfe056abf02330a
townivan <townivan@users.noreply.github.com>
2026-05-08 16:56:27 -0400

    Update Col_b_center.tsx

--- src/layout/Col_b_center/Col_b_center.tsx
+++ src/layout/Col_b_center/Col_b_center.tsx
@@ -5,20 +5,31 @@ import { hydrateTemplate } from "../../utils/template";
 import BlueprintPreview from "../../components/BlueprintPreview/BlueprintPreview";
 
 const Col_b_center = () => {
-    const instances = useEmailStore((state) => state.instances);
+  const instances = useEmailStore((state) => state.instances);
 
-    return (
-        <div className={styles.colCenter}>
-            <div className="canvas">
-                {instances.map((instance) => {
-                    const blueprint = Object.entries(BLUEPRINTS).find(([id]) => id === instance.blueprintId)?.[1];
-                    if (!blueprint) return null;
-                    const hydratedHtml = hydrateTemplate(blueprint.template, instance.data);
-                    return <BlueprintPreview key={instance.id} html={hydratedHtml} />;
-                })}
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.colCenter}>
+      <div className={styles.canvas}>
+        {instances.map((instance) => {
+          const blueprint = Object.entries(BLUEPRINTS).find(
+            ([id]) => id === instance.blueprintId,
+          )?.[1];
+          if (!blueprint) return null;
+          const hydratedHtml = hydrateTemplate(
+            blueprint.template,
+            instance.data,
+          );
+          return (
+            <BlueprintPreview
+              key={instance.id}
+              html={hydratedHtml}
+              isThin={instance.isThin}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Col_b_center;
