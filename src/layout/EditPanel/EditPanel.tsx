642eca73905856476a99849c5687c09d5f037be0
townivan <townivan@users.noreply.github.com>
2026-05-11 21:03:55 -0400

    Update EditPanel.tsx

--- src/layout/EditPanel/EditPanel.tsx
+++ src/layout/EditPanel/EditPanel.tsx
@@ -3,20 +3,20 @@ import styles from "./EditPanel.module.css";
 import { useEmailStore } from "../../store";
 
 const EditPanel = () => {
-    const toggleEditPanel = useEmailStore((state) => state.toggleEditPanel);
+  const closeEditPanel = useEmailStore((state) => state.closeEditPanel);
 
-    return (
-        <div className={styles.editPanel}>
-            <div className={styles.header}>
-                <div className={styles.col1}>EditPanel</div>
-                <div className={styles.col2}>
-                    <button type="button" onClick={toggleEditPanel}>
-                        <FiXCircle />
-                    </button>
-                </div>
-            </div>
+  return (
+    <div className={styles.editPanel}>
+      <div className={styles.header}>
+        <div className={styles.col1}>EditPanel</div>
+        <div className={styles.col2}>
+          <button type="button" onClick={closeEditPanel}>
+            <FiXCircle />
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default EditPanel;
