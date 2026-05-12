5daeb39445187921e22748f08c30cf12fa7fb200
townivan <townivan@users.noreply.github.com>
2026-05-11 21:03:38 -0400

    Update Col_c_right.tsx

--- src/layout/Col_c_right/Col_c_right.tsx
+++ src/layout/Col_c_right/Col_c_right.tsx
@@ -1,7 +1,22 @@
+import { FiCornerDownLeft } from "react-icons/fi";
 import styles from "./Col_c_right.module.css";
+import { useEmailStore } from "../../store";
 
 const Col_c_right = () => {
-    return <div className={styles.colRight}>3</div>;
+  const closeEditPanel = useEmailStore((state) => state.closeEditPanel);
+
+  return (
+    <div className={styles.colRight}>
+      <div className={styles.header}>
+        <div>
+          <button type="button" onClick={closeEditPanel}>
+            <FiCornerDownLeft />
+          </button>
+        </div>
+        <div>Edit Panel</div>
+      </div>
+    </div>
+  );
 };
 
 export default Col_c_right;
